821ebae4b1734b7e285cf300930fefca48e34969
Scriptsden <racharlakoushik@gmail.com>
2016-07-15 19:52:53 +0530

    test

--- logistic.R
+++ logistic.R
@@ -1,9 +1,10 @@
-
+#Tests
 # Required pakages
 library(aod)
 library(ggplot2)
 library(Rcpp)
-
+ x <- c(1,2,3)
+ y <- c(4,65)
 # Loading Data.
 mydata <- read.csv("http://www.ats.ucla.edu/stat/data/binary.csv")
 
